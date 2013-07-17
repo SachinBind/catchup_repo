266145ddb669302786c9215f7898b64ef539d0a2
Jacob MacIntyre <jacob@takeitaway.com>
2013-07-16 20:04:08 -0400

    jakes initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Catchup::Application.config.secret_key_base = '4117ce312ac3171afe4b55c86e157c1ff7a2f6536b11322bac63268e38ec4719e810cf45aaecc68d7e62ed9823ec6a8a706a65a157e18220749783844a73d072'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Catchup::Application.config.session_store :cookie_store, key: '_catchup_session'
